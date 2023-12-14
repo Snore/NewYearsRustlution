53863b9b07fff464193d8f3881351cca98c62da8
Paul Adams <padams@dekaresearch.com>
2023-12-14 19:43:17 +0000

    solving aoc8

--- aoc_8/src/main.rs
+++ aoc_8/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::env;
 use std::collections::HashMap;
+use std::time::Instant;
 
 #[derive(Debug)]
 
@@ -36,6 +37,10 @@ impl Directions {
         self.counter = (self.counter + 1) % self.sequence.len();
         &self.sequence[next_move]
     }
+
+    pub fn reset( &mut self ) {
+        self.counter = 0;
+    }
 }
 
 #[derive(Debug)]
@@ -61,12 +66,16 @@ impl Map {
     }
 
     /// Returns the destination given the starting junction and the direction to go
-    pub fn get_destination( &self, current_loc: &String, direction: &Turn ) -> String {
-        let junction = self.paths.get(current_loc).unwrap();
+    pub fn get_destination( &self, current_loc: &String, direction: &Turn ) -> Option<String> {
+        let junction = self.paths.get(current_loc)?;
         match direction {
-            Turn::Left => junction.left.clone(),
-            Turn::Right => junction.right.clone(),
+            Turn::Left => Some(junction.left.clone()),
+            Turn::Right => Some(junction.right.clone()),
         }
+    }
+
+    pub fn get_all_nodes( &self ) -> Vec<String> {
+        self.paths.keys().map(|n| n.clone()).collect()
     }
 
     /// Parses a line from the map section of the input
@@ -95,16 +104,54 @@ fn main() {
     let mut my_directions: Directions = Directions::parse(directions_raw);
     let my_map: Map = Map::parse(map_raw);
 
-   //  println!("{:?}\n{:?}", my_directions.sequence, my_map.paths);
-
     // solve part 1
     let target_location: String = "ZZZ".to_string();
     let mut current_location: String = "AAA".to_string();
-    let mut steps: u32 = 0;
+    let mut steps: u64 = 0;
     while current_location != target_location {
-       current_location = my_map.get_destination(&current_location, my_directions.get_next_step());
-       steps += 1;
+        let destination = my_map.get_destination(&current_location, my_directions.get_next_step());
+        match destination {
+            Some(destination) => {
+                current_location = destination;
+                steps += 1;
+            },
+            None => {
+                steps = 0;
+                break; // unsolvable. just break out
+            },
+        }
     }
 
-    println!("Number of moves needed is [{steps}]");
+    println!("Number of moves needed for part 1 is [{steps}]");
+
+    // solve part 2
+    steps = 0;
+    my_directions.reset();
+    let timing_start: Instant = Instant::now();
+    let mut current_locations: Vec<String> = my_map.get_all_nodes()
+                                                   .into_iter()
+                                                   .filter(|n| n.ends_with('A'))
+                                                   .collect();
+    while !current_locations.iter()
+                            .all(|l| l.ends_with('Z')) {
+        // note the turn to take so that all nodes can use the same direction in the same step
+        let next_turn: &Turn = my_directions.get_next_step();
+        current_locations.iter_mut()
+                         .for_each(|l| *l = my_map.get_destination(l, next_turn).unwrap());
+        steps += 1;
+    }
+
+    // Idea:
+    // this takes too long.  maybe i can identify cycles and once i establish the cycles of all starting nodes
+    // I can just find the cycle they all share by multiplying them all together.
+
+    // run single runner two steps
+    // compare first half of array and back half
+    // repeat until above is satisfied
+    // cut recording in half, don't need the duplicate.
+    // find the indecies that end in 'Z'
+    // ???
+
+    let elapsed: std::time::Duration = timing_start.elapsed();
+    println!("Number of ghost moves needed for part 2 is [{steps}] spooky steps.\nThis took {elapsed:?} to complete.");
 }
