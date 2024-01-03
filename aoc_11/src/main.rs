4d8e8a45a6369f63fc9d52b987af5a620d0e5055
Paul Adams <padams@dekaresearch.com>
2024-01-03 13:57:59 +0000

    added input for 11

--- aoc_11/src/main.rs
+++ aoc_11/src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // get input
+    let args: Vec<String> = env::args().collect();
+    let file_path: &String = &args[1];
+
+    // read
+    let space_raw: String = fs::read_to_string(file_path).unwrap();
+    println!("{space_raw}");
 }
